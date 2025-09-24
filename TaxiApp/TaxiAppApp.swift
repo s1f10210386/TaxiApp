f0ff8b0abdc0584db5d3b6fe0ab56f06343c35d5
hotaka <s1f102103864@iniad.org>
2025-09-24 21:11:19 +0900

    initial commit

---
+++ TaxiApp/TaxiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiAppApp.swift
+//  TaxiApp
+//
+//  Created by 金澤帆高 on 2025/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TaxiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
